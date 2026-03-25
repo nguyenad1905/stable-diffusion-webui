23108bc0e28dd6b034b142e1d7d00df197417fba
QuangAnh <nguyenad1905@gmail.com>
2026-03-25 17:20:44 +0700

    config: Set `STABLE_DIFFUSION_REPO` and configure `COMMANDLINE_ARGS` for CPU-only execution.

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set STABLE_DIFFUSION_REPO=https://github.com/w-e-w/stablediffusion.git
+set COMMANDLINE_ARGS=--use-cpu all --skip-torch-cuda-test --no-half --precision full
 
 call webui.bat
