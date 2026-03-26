b4f7390f63661c2cc2c081a2d457059e68e7e0c7
QuangAnh <nguyenad1905@gmail.com>
2026-03-26 09:40:23 +0700

    COMMANDLINE_ARGS GPU

--- webui-user.bat
+++ webui-user.bat
@@ -4,6 +4,7 @@ set PYTHON=
 set GIT=
 set VENV_DIR=
 set STABLE_DIFFUSION_REPO=https://github.com/w-e-w/stablediffusion.git
-set COMMANDLINE_ARGS=--use-cpu all --skip-torch-cuda-test --no-half --precision full
+@REM set COMMANDLINE_ARGS=--use-cpu all --skip-torch-cuda-test --no-half --precision full
+set COMMANDLINE_ARGS=
 
 call webui.bat
