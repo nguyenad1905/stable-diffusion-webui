1eed6299ba666acdf4a06e3a0c41896e8b6a8c0b
QuangAnh <nguyenad1905@gmail.com>
2026-03-31 09:35:00 +0700

    feat: add ui_extensions module for managing extension states and configuration backups

--- modules/ui_extensions.py
+++ modules/ui_extensions.py
@@ -379,12 +379,12 @@ def install_extension_from_url(dirname, url, branch_name=None):
         try:
             os.rename(tmpdir, target_dir)
         except OSError as err:
-            if err.errno == errno.EXDEV:
-                # Cross device link, typical in docker or when tmp/ and extensions/ are on different file systems
+            if err.errno in (errno.EXDEV, errno.EACCES):
+                # Cross device link or Access Denied (typical on Windows if files are temporarily locked)
                 # Since we can't use a rename, do the slower but more versatile shutil.move()
                 shutil.move(tmpdir, target_dir)
             else:
-                # Something else, not enough free space, permissions, etc.  rethrow it so that it gets handled.
+                # Something else, not enough free space, etc.  rethrow it so that it gets handled.
                 raise err
 
         import launch
